670ff16f64b25b413190362ef99dd7f387bf6b2f
Chelsea Maria John <john2@jwlogin21.juwels>
2022-07-15 14:10:40 +0200

    initial commit

---
+++ jobscript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=opengptx-elm
+#SBATCH --job-name="dlProfTest"
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --partition=develbooster
+#SBATCH --gres=gpu:1
+#SBATCH --output=test-out.%j
+#SBATCH --error=test-err.%j
+#SBATCH --time=00:10:00
+
+set -e 
+echo -e "Start `date +"%F %T"` | $SLURM_JOB_ID $SLURM_JOB_NAME | `hostname` | `pwd` \n" #log start time etc.
+
+srun apptainer exec --nv pytorch-21.11-py3.sif dlprof --mode=pytorch --reports=summary --iter_start=200 --iter_stop=400 python mnist_train.py
+
+echo -e "End `date +"%F %T"` | $SLURM_JOB_ID $SLURM_JOB_NAME | `hostname` | `pwd` \n" #log start time etc.
